41e03c0ed13b67197d40122c75024562fb2cc3a9
Koaxy <92256387+Koaxy@users.noreply.github.com>
2022-01-06 23:49:58 +0000

    Update GUI.java

--- MyGui/src/GUI.java
+++ MyGui/src/GUI.java
@@ -3,13 +3,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
 
